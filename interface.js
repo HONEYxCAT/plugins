6259d06a05e13814ec0a50c2e48e553252cbda67
HONEYxCAT <166288046+HONEYxCAT@users.noreply.github.com>
2025-10-25 14:12:28 +0300

    Remove old interface and add new interface styles

--- interface.js
+++ interface.js
@@ -307,17 +307,6 @@
 
 	function startPlugin() {
 		window.plugin_interface_ready = true;
-		var old_interface = Lampa.InteractionMain;
-		var new_interface = component;
-
-		Lampa.InteractionMain = function (object) {
-			var use = new_interface;
-			// if (!(object.source == "tmdb" || object.source == "cub")) use = old_interface;
-			// if (window.innerWidth < 767) use = old_interface;
-			// if (!Lampa.Account.hasPremium()) use = old_interface;
-			// if (Lampa.Manifest.app_digital < 153) use = old_interface;
-			return new use(object);
-		};
 
 		Lampa.Template.add(
 			"new_interface_style",
